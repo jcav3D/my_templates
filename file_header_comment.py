4ba2664484c4589d92dd7f2cbe6719ced9037785
jcav3D <j.cavalcante@lahorde.school>
2025-07-04 00:17:12 +0200

    Create file_header_comment.py

---
+++ file_header_comment.py
@@ -0,0 +1,24 @@
+# en tete de script / module docstring / docstring de module
+
+
+# ===========================================
+# Script Name: duplicate.py
+# Author: Jean Cavalcante
+# Date: 29/06/2025
+# Maya Version: 2022+
+# Python Version: 3.x
+#
+# Description:
+#     Ce script duplique un objet sélectionné dans Maya,
+#     applique un décalage en x et z pour chaque copie,
+#     et crée une animation de type "pulse" sur le scale de l'objet.
+#
+# Instructions:
+#     - Sélectionner un objet dans la scène.
+#     - Lancer le script dans l’éditeur de script Python de Maya.
+#
+# Dependencies:
+#     - Maya.cmds
+#     - random
+#
+# ===========================================
